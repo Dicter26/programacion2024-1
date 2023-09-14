d026885a6c7b5aefe441e1d9f9ccfae5ebf48b03
Dicter26 <dicter.r26@ciencias.unam.mx>
2023-09-13 22:38:50 -0600

    añadiendo practica 1

---
+++ operadores/practica1/Operadores.java
@@ -0,0 +1,48 @@
+public class Operadores{
+    
+    public static void main(String [] args){
+        
+        String saludo = "Soy una cadena";
+        System.out.println("Hola, " + saludo);
+        int alto= 5;
+        int ancho= 10;
+        int areaRectangulo = alto*ancho;
+        System.out.println("El area del rectangulo es: " +areaRectangulo);
+
+        /**escribe la suma de dos números y concatena el resultado en 
+         *la línea system.out.println, previamente se proporciono un ejemplo
+         * 
+         * Aqui va tu solución, borra este comentario.
+         * **/
+        System.out.println("Suma: " );
+
+        /**escribe la resta de dos números y concatena el resultado en 
+         *la línea system.out.println, previamente se proporciono un ejemplo
+         * 
+         * Aqui va tu solución, borra este comentario.
+         * **/
+        System.out.println("Resta: " );
+
+        /**escribe la división de dos números y concatena el resultado en 
+         *la línea system.out.println, previamente se proporciono un ejemplo
+         * 
+         * Aqui va tu solución, borra este comentario.
+         * **/
+        System.out.println("División: " );
+
+        /**Obten el volumen de un cilindro, la fórmula necesaria es
+         * V= ((r^2)*pi)*h
+         * 
+         * Aqui va tu solución, borra este comentario.
+         * **/
+        System.out.println("Volúmen del Cilindro: " );
+
+        /**Completa las variables con tu nombre y el titulo del libro que escribirias,
+         *usa la concatenación (+) para obtener un resultado similar:
+         *"moby dick fue escrito por herman melville"
+         * 
+         * Aqui va tu solución, borra este comentario
+         * **/
+        System.out.println("");
+        }
+    }
